9a11a6a811c6b4cce5a1f2ed4dbd173236429a3b
Mark Hanford <mark@hanfordonline.co.uk>
2015-08-06 14:59:02 +0100

    Lockdown user/add.

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -10,7 +10,7 @@ class UsersController extends AppController
     public function beforeFilter(Event $event)
     {
         parent::beforeFilter($event);
-        $this->Auth->allow('add', 'logout');
+        $this->Auth->allow('logout');
     }
 
      public function index()
