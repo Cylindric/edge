53dfc0a0ea17a58f9535bf159ab34c803f3d05f0
Cylindric <mark@hanfordonline.co.uk>
2015-08-07 13:22:10 +0100

    Deploy test.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,3 +3,17 @@ yum -y install httpd24 > /var/log/installapache.out 2>&1
 yum -y install mysql php5-mysql > /var/log/installmysql.out 2>&1
 yum -y install php55 php55-intl php55-mysqlnd > /var/log/installphp.out 2>&1
 yum -y install git expect > /var/log/installother.out 2>&1
+curl -s https://getcomposer.org/installer | php
+
+
+cat > /etc/httpd/conf.d/edge.conf <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/edge/htdocs/webroot"
+    ServerName edge.hanfordonline.co.uk
+    ErrorLog "/var/www/edge/log/htdocs/error.log"
+    CustomLog "/var/www/edge/log/htdocs/access.log" common
+    <Directory "/var/www/edge/htdocs/webroot">
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
