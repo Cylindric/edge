1cde79b8a4b1e3b4a58908e0c203048994338712
Cylindric <mark@hanfordonline.co.uk>
2015-10-29 00:51:41 +0000

    Remove redundant methods and comments.

--- App/src/Model/Entity/Group.php
+++ App/src/Model/Entity/Group.php
@@ -11,31 +11,4 @@ class Group extends Entity
         '*' => true,
         'id' => false,
     ];
-
-    public function _getObligation() {
-        $chars = TableRegistry::get('Characters');
-
-        $query = $chars->find();
-        $query->select(['total' => $query->func()->sum('obligation')])
-            ->where(['group_id' => $this->id]);
-        return $query->toArray()[0]->total;
-    }
-
-    public function _getCredits() {
-        $chars = TableRegistry::get('Characters');
-
-        $query = $chars->find();
-        $query->select(['total' => $query->func()->sum('credits')])
-            ->where(['group_id' => $this->id]);
-        return $query->toArray()[0]->total;
-    }
-
-    public function _getXp() {
-        $chars = TableRegistry::get('Characters');
-
-        $query = $chars->find();
-        $query->select(['total' => $query->func()->sum('xp')])
-            ->where(['group_id' => $this->id]);
-        return $query->toArray()[0]->total;
-    }
 }
