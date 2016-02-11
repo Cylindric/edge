fbeba6c1ff837eafdec71563b619116722df2779
Cylindric <mark@hanfordonline.co.uk>
2016-02-11 20:10:30 +0000

    Fixed some tests

---
+++ App/tests/Fixture/CharactersCareersFixture.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class CharactersCareersFixture extends TestFixture
+{
+    public $import = ['table' => 'characters_careers'];
+
+    public $records = [
+    ];
+}
