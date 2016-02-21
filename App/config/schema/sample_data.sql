b1d4cc7a770e9ec69da9faf59d719b03c6a81660
Cylindric <mark@hanfordonline.co.uk>
2016-02-21 20:25:28 +0000

    Fix broken group-join function for characters

--- App/config/schema/sample_data.sql
+++ App/config/schema/sample_data.sql
@@ -4,7 +4,7 @@ SELECT @dodge_id:=`id` FROM talents WHERE name='Dodge';
 SELECT @blaster_id:=`id` FROM weapons WHERE name='Blaster Carbine';
 SELECT @laminate_id:=`id` FROM armour WHERE name='Laminate';
 
-DELETE FROM characters WHERE `name` = 'Default Test User' AND `user_id` = @admin_id;
+-- DELETE FROM characters WHERE `name` = 'Default Test User' AND `user_id` = @admin_id;
 
 INSERT INTO characters (`name`, `user_id`, `species_id`, `created`, `modified`) VALUES ('Default Test User', @admin_id, @human_id, NOW(), NOW());
 SET @character_id:=LAST_INSERT_ID();
@@ -14,6 +14,11 @@ INSERT INTO characters_weapons (`character_id`, `weapon_id`, `created`, `modifie
 INSERT INTO characters_armour (`character_id`, `armour_id`, `created`, `modified`) VALUES (@character_id, @laminate_id, NOW(), NOW());
 INSERT INTO credits (`character_id`, `value`, `note`, `created`, `created_by`, `modified`, `modified_by`) VALUES (@character_id, '123', 'Some Credits', NOW(), @admin_id, NOW(), @admin_id);
 INSERT INTO xp (`character_id`, `value`, `note`, `created`, `created_by`, `modified`, `modified_by`) VALUES (@character_id, '10', 'Some XP', NOW(), @admin_id, NOW(), @admin_id);
+
+INSERT INTO groups (`name`, `created`, `modified`) VALUES ('Test Group', NOW(), NOW());
+SET @group_id:=LAST_INSERTED_ID();
+
+INSERT INTO characters_groups (`character_id`, `group_id`) VALUES (@character_id, @group_id);
 
 SELECT * FROM characters WHERE id=@character_id;
 SELECT * FROM characters_talents WHERE character_id=@character_id;
